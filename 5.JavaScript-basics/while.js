48fc4af2cb52bbcad42c957b0524032402922c69
allu@po.os <aleksanteri.palomaki@tuni.fi>
2025-03-27 14:35:52 +0200

    add week 5

---
+++ 5.JavaScript-basics/while.js
@@ -0,0 +1,9 @@
+//Create a while loop, that counts down from 5 to 0 (print the numbers using console.log), and after the loop it prints "Lift off!" to the console
+
+let i = 5;
+
+while(i >= 0){
+    console.log(i);
+    i--;
+}
+console.log("Lift off!");
